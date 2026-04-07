edb8a646cecc1e3513fe8d18017c5730e3a28aa6
Nikolett <tillnikolett@gmail.com>
2026-04-07 18:02:56 +0200

    Fix gradlew.bat classpath

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,4 @@
-@rem
+﻿@rem
 @rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
@@ -67,7 +67,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%DIRNAME%gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
